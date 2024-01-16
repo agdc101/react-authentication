0b68ecc8bfe61e0d7e8f72f5834ac6e5de04365c
agdc101 <agdc101@gmail.com>
2024-01-16 19:52:14 +0000

    feat: new and events routes protected

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,7 +1,17 @@
+import { redirect } from 'react-router-dom';
+
 export function getAuthToken() {
   return localStorage.getItem('token');
 }
 
 export function setAuthToken(token) {
   return getAuthToken();
+}
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+  if (!token) {
+    throw redirect('/auth');
+  }
+  return null;
 }
